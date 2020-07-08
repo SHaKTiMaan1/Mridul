c5392cdd0fa0c53256906eb925326318adf10125
Mridul Sharma <62736480+Mridul001@users.noreply.github.com>
2020-07-08 14:41:44 +0530

    Comments updated

--- routes/cciRoutes.js
+++ routes/cciRoutes.js
@@ -3,9 +3,9 @@
 
 var express = require("express"),
     router = express.Router(),
-    Cci = require("../models/cci2.js"), //This is the model for CCI collection. The collection where CCI Data get stored after registration.
-    Child = require("../models/child.js"), // This is the model for Child collection.The collection where child Data get stored after registration. 
-    attendance = require("../models/attendance.js"); //This is model for attendance collection.   
+    Cci = require("../models/cci2.js"),
+    Child = require("../models/child.js"),
+    attendance = require("../models/attendance.js");
 
     
 // CCI Registration
@@ -15,8 +15,7 @@ router.get('/addCci', function (req, res) {
 
 // CCi details Page GET Request
 router.get('/cciInfo', function (req, res) {
-//Find function on Cci collection, this will return all cci in dattabase to variable allCci which wil be passed to variable "cci" in ejs template in res.render function.
-    
+
   Cci.find({}, function (err, allCci) {
     if (err) {
       console.log(err);
@@ -29,7 +28,7 @@ router.get('/cciInfo', function (req, res) {
           console.log(error);
         }
         else
-        {//After finding allchildren and passing it to variable "child" we will pass these variables to "cci" and "child" variable in cciInfi.ejs.
+        {
           res.render('cciInfo.ejs', { cci: allCci, child: child });
         }
       });
@@ -54,17 +53,18 @@ router.post('/addCci', function (req, res) {
   // This function finds the latest cci added and return its count value which is udes to make CCI-ID
 
   Cci.findOne({ "cci_address.district": req.body.district }, 'cci_name count')
-    .sort('-count')// this function find the ccis of particular district and give name and count of the cci of max count value.
-    .exec(function (err, result) {// Saving the cci in result  variable.
+    .sort('-count')
+    .exec(function (err, latest) {
       if (err) {
         console.log(err);
       }
       else {
-        console.log(result.count); // This count means the number of CCI already in the district.
-        let count = result.count;
+        console.log(latest.count);
+        let count = latest.count;
         count++;
-        console.log(count); 
+        console.log(count); // This count means the number of CCI already in the district.
         // Logic for the cci_id
+
         ID = distname.concat(cciname, count);
         console.log(ID);
 
@@ -103,13 +103,13 @@ router.post('/addCci', function (req, res) {
 
       }
       let Cciname = req.body.name;
-      res.redirect('/ccisuccess/' + ID + '/' + Cciname);//Cciname and ID will be used in success page for reteiving data of a particular CCI.
+      res.redirect('/ccisuccess/' + ID + '/' + Cciname);//Cciname and ID will be used in success page.
     });
 
 
 });
 
-
+//For the success page.
 router.get('/ccisuccess/:id/:name', function (req, res) {
   res.render("cciregsuccess.ejs", { id: req.params.id, name: req.params.name}
   );
@@ -144,7 +144,7 @@ router.get('/ccisuccess/:id/:name', function (req, res) {
 
 
 
-//Roue for getting atendance of a CCI
+// this is the request when we click the button of attendance details on CCI INFO page.
 router.get('/attendance/:cciId/:cciname', function(req, res){
   let cciid = req.params.cciId;
   // console.log(cciid);
@@ -155,13 +155,14 @@ router.get('/attendance/:cciId/:cciname', function(req, res){
     else
     {
       console.log(allchild);
-      res.render('attendance.ejs', {child: allchild , cci_name:req.params.cciname});
+      res.render('attendance.ejs', {child: allchild , cci_name:req.params.cciname}); 
+      // Here we are passing the child and cci information for ejs page 
     }
   });
 
 });
 
-
+// this requst is just for testing . No use in the web app functioning. 
 router.get('/getdetails', function(req, res){
   attendance.find({}, function(err, data){
     if(err)
