a479d3ac860523131b9beac71a8034fb84915452
Mridul Sharma <62736480+Mridul001@users.noreply.github.com>
2020-07-16 22:30:16 +0530

    iteration 2

--- models/child.js
+++ models/child.js
@@ -1,21 +1,29 @@
 var mongoose = require("mongoose");
 
 var ChildSchema = new mongoose.Schema({
-    name        : String,
-    C_Id        : String,
+    fname        : String,
+    lname        : String,
+    C_Id        : String, //ChildId
+    number       : Number,// This number shows the nth child in CCi. Serial number of child admitted to this cci.
     age         : Number,
+    dob         : {type: Date },// When Child is registered again after investigation is done.
     cci_name    : String,
     cci_id      : String,
-
-    cci_address : {
-                  address : String, 
-                  district: String,
-                  state: String
+    status      : String,// O, A, S
+    guardian_details : {
+        Guardian_1 : {
+            name: String,
+            age: Number
+        }, 
+        Guardian_2 : {
+            name: String,
+            age: Number
+        } 
     },
 
-    reg_date    : {type : Date},
+    reg_date    : {type : Date},//date when found and registered for first time.
     gender      : String,
-    witness     : String
+    witness     : String// CWC Official Witness ID
 });
 
 module.exports = mongoose.model("Child", ChildSchema);
